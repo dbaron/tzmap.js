d6871b3e04360cf0f1f44707234d815fdd473354
L. David Baron <dbaron@dbaron.org>
2011-12-23 14:06:24 -0500

    Add a fast zoneContains API since zoneAt is built on it, and use it for better tests.

--- tzmap.js
+++ tzmap.js
@@ -87,92 +87,107 @@
         }
     }
 
-    var public_zoneAt = function(lat, lon) {
+    function zoneContains(tzid, lat, lon) {
         var zones = gJSON.zones;
         var chains = gJSON.chains;
 
+        var zone = zones[tzid];
+        for (var regionIdx in zone) {
+            var region = zone[regionIdx];
+
+            // Since we don't need to worry about zones containing the
+            // north pole (FIXME: really?), we can just count the number
+            // of times that a north-south line from the north pole to
+            // our point intersects the zone.
+            // Furthermore, the segments are short enough that we don't
+            // need to worry about whether they're great circle lines or
+            // lines on an easier projection.
+            var intersects = 0;
+            for (var chainIdx in region) {
+                var chainobj = region[chainIdx];
+                var chainID = chainobj[0];
+                var chainInv = chainobj[1];
+                var chain = chains[chainID];
+                var prevlon, prevlat;
+                for (var pointIdx in chain) {
+                    var pointobj = chain[pointIdx];
+                    var ptlon = pointobj[0];
+                    var ptlat = pointobj[1];
+                    if (pointIdx > 0) {
+                        if (ptlon == prevlon) {
+                            // vertical line.  All we need to do is
+                            // check if our point is *on* it.
+                            if (ptlon == lon
+                                && in_range(lat, ptlat, prevlat))
+                                return true;
+                        } else {
+                            var alon, ptalon, prevalon;
+                            if (Math.abs(ptlon - prevlon) > 180) {
+                                // this segment crosses the date line,
+                                // so use adjusted numbers instead
+                                alon = (lon + 180) % 360;
+                                ptalon = (ptlon + 180) % 360;
+                                prevalon = (prevlon + 180) % 360;
+                            } else {
+                                alon = lon;
+                                ptalon = ptlon;
+                                prevalon = prevlon;
+                            }
+
+                            if (ptalon < prevalon) {
+                                var tmp = ptalon;
+                                ptalon = prevalon;
+                                prevalon = tmp;
+                            } else {
+                            }
+
+                            // Check the endpoint at the west end
+                            // but not the east end.  This does the
+                            // right thing for the intersects count,
+                            // since we need to worry about both
+                            // what happens when the segments turn
+                            // back the opposite east/west or
+                            // continue the same east/west from the
+                            // boundary.  FIXME: But it's not quite
+                            // right for the on-the-line check.
+                            if (prevalon <= alon && alon < ptalon) {
+                                var xlat = prevlat + (ptlat - prevlat) * ((alon - prevalon) / (ptalon - prevalon));
+                                if (xlat == lat)
+                                    // on the line
+                                    return true;
+                                if (xlat > lat) {
+                                    ++intersects;
+                                }
+                            }
+                        }
+                    }
+                    prevlon = ptlon;
+                    prevlat = ptlat;
+                }
+            }
+            if (intersects % 2 == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    var public_zoneContains = function(zone, lat, lon) {
         if (lat >= 90 || lat <= -90)
             return null;
         lon = ((lon % 360) + 180) % 360 - 180;
 
-        for (var tzid in zones) {
-            var zone = zones[tzid];
-            for (var regionIdx in zone) {
-                var region = zone[regionIdx];
+        return zoneContains(zone, lat, lon);
+    }
 
-                // Since we don't need to worry about zones containing
-                // the north pole (FIXME: really?), we can just count
-                // the number of times that a north-south line from the
-                // north pole to our point intersects the zone.
-                // Furthermore, the segments are short enough that we
-                // don't need to worry about whether they're great
-                // circle lines or lines on an easier projection.
-                var intersects = 0;
-                for (var chainIdx in region) {
-                    var chainobj = region[chainIdx];
-                    var chainID = chainobj[0];
-                    var chainInv = chainobj[1];
-                    var chain = chains[chainID];
-                    var prevlon, prevlat;
-                    for (var pointIdx in chain) {
-                        var pointobj = chain[pointIdx];
-                        var ptlon = pointobj[0];
-                        var ptlat = pointobj[1];
-                        if (pointIdx > 0) {
-                            if (ptlon == prevlon) {
-                                // vertical line.  All we need to do is
-                                // check if our point is *on* it.
-                                if (ptlon == lon
-                                    && in_range(lat, ptlat, prevlat))
-                                    return tzid;
-                            } else {
-                                var alon, ptalon, prevalon;
-                                if (Math.abs(ptlon - prevlon) > 180) {
-                                    // this segment crosses the date line,
-                                    // so use adjusted numbers instead
-                                    alon = (lon + 180) % 360;
-                                    ptalon = (ptlon + 180) % 360;
-                                    prevalon = (prevlon + 180) % 360;
-                                } else {
-                                    alon = lon;
-                                    ptalon = ptlon;
-                                    prevalon = prevlon;
-                                }
+    var public_zoneAt = function(lat, lon) {
+        if (lat >= 90 || lat <= -90)
+            return null;
+        lon = ((lon % 360) + 180) % 360 - 180;
 
-                                if (ptalon < prevalon) {
-                                    var tmp = ptalon;
-                                    ptalon = prevalon;
-                                    prevalon = tmp;
-                                } else {
-                                }
-
-                                // Check the endpoint at the west end
-                                // but not the east end.  This does the
-                                // right thing for the intersects count,
-                                // since we need to worry about both
-                                // what happens when the segments turn
-                                // back the opposite east/west or
-                                // continue the same east/west from the
-                                // boundary.  FIXME: But it's not quite
-                                // right for the on-the-line check.
-                                if (prevalon <= alon && alon < ptalon) {
-                                    var xlat = prevlat + (ptlat - prevlat) * ((alon - prevalon) / (ptalon - prevalon));
-                                    if (xlat == lat)
-                                        // on the line
-                                        return tzid;
-                                    if (xlat > lat) {
-                                        ++intersects;
-                                    }
-                                }
-                            }
-                        }
-                        prevlon = ptlon;
-                        prevlat = ptlat;
-                    }
-                }
-                if (intersects % 2 == 1) {
-                    return tzid;
-                }
+        for (var tzid in gJSON.zones) {
+            if (zoneContains(tzid, lat, lon)) {
+                return tzid;
             }
         }
         return null;
@@ -193,6 +208,15 @@
          * success_callback has been called.
          */
         loadData: public_loadData,
+
+        /**
+         * zoneContains(zone, lat, lon)
+         *
+         * Return whether the time zone name (e.g.,
+         * "America/Los_Angeles") contains the point at the given
+         * latitude and longitude or has that point on its boundary.
+         */
+        zoneContains: public_zoneContains,
 
         /**
          * zoneAt(lat, lon)
